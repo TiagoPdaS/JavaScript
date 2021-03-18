7056ccc5f4c1208c4c52da586f8ccc9ae9415326
TiagoPdaS <tiagopdasgithub@gmail.com>
2021-03-17 20:25:38 -0400

    dom
    iniciando com DOM

---
+++ DOM/script.js
@@ -0,0 +1,10 @@
+var p = document.getElementsByTagName('p');
+
+//p eh um objeto.
+
+p[0].innerHTML = 'manipulado via JS';
+
+for(var i = 0; i < p.length; i++){
+    p[i].innerHTML = 'manipulado via JS'
+}
+
